9e8f3fe13cf72a261d8b40fa23c64f7bcea7a7d7
Дмитрий Кочетыгов <kochetygovu1@tk34.mskobr.ru>
2022-04-11 13:29:01 +0600

    Техника предположения делегата

---
+++ DelegatePredpTechnic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DelegatePredpTechnic
+{
+    class Program
+    {
+        public delegate int StrDelegate(string s);
+        static void Main(string[] args)
+        {
+            StrDelegate sD = delegate (string str)
+            {
+                if (str == null)
+                    throw new Exception("Строка отсутствует");
+                else
+                    return str.Length;
+            };
+            Console.WriteLine(sD(""));
+
+            Console.ReadKey();
+        }
+    }
+}
